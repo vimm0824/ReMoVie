f87310db61758babbe2048c91353855776e11857
test <test@test.com>
2024-02-22 16:43:20 +0900

    #240222 [Test] Spring boot 환경세팅

---
+++ ReMoVie/src/test/java/com/ReMoVie/ReMoVieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ReMoVie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReMoVieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
