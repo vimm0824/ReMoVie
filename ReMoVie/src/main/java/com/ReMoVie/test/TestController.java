301cb2439651a4082bbebbb6c65676e106527e3a
test <test@test.com>
2024-02-29 23:15:27 +0900

    #240229 [Feat] TMDB open API

--- ReMoVie/src/main/java/com/ReMoVie/test/TestController.java
+++ ReMoVie/src/main/java/com/ReMoVie/test/TestController.java
@@ -1,11 +1,16 @@
 package com.ReMoVie.test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ReMoVie.infra.openApi.TmdbOpenApi;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,6 +19,7 @@ import lombok.RequiredArgsConstructor;
 public class TestController {
 	
 	private final TestService testService;
+	private final TmdbOpenApi tmdbOpenApi;
 	
 	@ResponseBody
     @GetMapping("/test1")
@@ -34,5 +40,17 @@ public class TestController {
   	@GetMapping("/test3")
   	public Test test3() {
   		return testService.test(1L);
+  	}
+  	
+  	@ResponseBody
+  	@GetMapping("/test4/{search}")
+  	public List<Map<String, Object>> test4(
+  			@PathVariable("search") String search
+  			) {
+  		List<Map<String, Object>> result = new ArrayList<>();
+  		
+  		result = tmdbOpenApi.getTmdbSearch(search);
+  		
+  		return result;
   	}
 }
